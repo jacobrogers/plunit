4c39d917bd5904c9b5ef673dec1a6f9bda61a807
removedporn <86824510+removedporn@users.noreply.github.com>
2021-08-12 19:54:57 +0800

    Delete core-runner directory

--- core-runner/src/main/java/plunit/observers/DefaultPopUp.java
+++
@@ -1,7 +0,0 @@
-package plunit.observers;
-
-public class DefaultPopUp implements PopUp {
-	public void show(String message) {}
-	public void show(Exception e) {}
-	public void show(RuntimeException e) {}
-}
--- core-runner/src/main/java/plunit/observers/PopUp.java
+++
@@ -1,7 +0,0 @@
-package plunit.observers;
-
-public interface PopUp {
-	public abstract void show(String message);
-	public abstract void show(Exception e);
-	public abstract void show(RuntimeException e);
-}
