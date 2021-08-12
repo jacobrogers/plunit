a3fb1b701de2e0d4f6018650da15ba82cf6ac32e
removedporn <86824510+removedporn@users.noreply.github.com>
2021-08-12 20:10:25 +0800

    Delete core-runner directory

--- core-runner/src/main/java/plunit/observers/TestSuiteListener.java
+++
@@ -1,9 +0,0 @@
-package plunit.observers;
-
-import plunit.PlunitTestSuite;
-
-public interface TestSuiteListener {
-   public abstract void notify( PlunitTestSuite testSuite, int testNumber);
-   public abstract void notifyStart(int numberOfTests);
-   public abstract void notifyFinish();
-}
